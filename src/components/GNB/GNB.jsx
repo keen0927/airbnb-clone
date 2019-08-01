d437e7f60fbaf8fb24be737cef945150555eaeaf
keen <kyh0927@within.co.kr>
2019-08-01 19:24:49 +0900

    hook scroll animate

--- src/components/GNB/GNB.jsx
+++ src/components/GNB/GNB.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
@@ -10,21 +10,65 @@ const Button = styled.button`
     ${props => props.ddd && css`
         background: purple;
     `}
-`
+`;
+
+const GnbElement = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    /* height: 50px; */
+    /* height: ${props => props.animateHide > 100 ? '200px' : '50px' || '50px'}; */
+    /* height: ${props => props.animateHide}; */
+    height: 50px;
+    z-index: 10;
+    background: rgba(0,0,0,0.2);
+    transition: 0.5s;
+    ${props => props.animateHide && css`
+        height: 100px;
+    `}
+`;
+
+const useScroll = () => {
+    const [scrollValueY, setScrollValueY] = useState(false);
+
+    const onScroll = () => {
+        let windowScrollY = window.scrollY;
+        if (windowScrollY > 100) {
+            setScrollValueY(true);
+        } else {
+            setScrollValueY(false);
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
+    return scrollValueY;
+}
 
 const GNB = () => {
 
     const [buttonValue, setButtonValue] = useState(false);
+
+    const scrollValueY = useScroll();
 
     const onChangeButton = () => {
         setButtonValue(!buttonValue);
     }
 
     return (
-        <div>
-            GNB
-            <Button ddd={buttonValue}>버튼</Button>
-            <Button onClick={onChangeButton}>변경버튼</Button>
+        <div style={{ height: '5000px' }}>
+
+
+
+            <GnbElement animateHide={scrollValueY}>
+                <p>Y : {scrollValueY}</p>
+            </GnbElement>
+
+            {/*<Button ddd={buttonValue}>버튼</Button>*/}
+            {/*<Button onClick={onChangeButton}>변경버튼</Button>*/}
 
         </div>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import GNB from '../../components/GNB';
-import StyledComponent from '../../components/StyledComponentTest';
+// import StyledComponent from '../../components/StyledComponentTest';
 
 const Home = () => {
     return (
         <div>
             <GNB />
-            <StyledComponent />
+            {/*<StyledComponent />*/}
         </div>
     )
 }
 
-export default Home
+export default Home;

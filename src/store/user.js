0bc13a3a57ea4456808e4c52596bf249be7a700e
keen <kyh0927@within.co.kr>
2019-08-13 13:30:55 +0900

    redux add

---
+++ src/store/user.js
@@ -0,0 +1,38 @@
+const LOG_IN = 'LOG_IN';
+const LOG_OUT = 'LOG_OUT';
+
+export const login = () => {
+    return {
+        type: LOG_IN
+    }
+}
+
+export const logout = () => {
+    return {
+        type: LOG_OUT
+    }
+}
+
+const initialState = {
+    isLoggedin: false
+}
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case LOG_IN:
+            return {
+                ...state,
+                isLoggedin: true
+            }
+        case LOG_OUT:
+            return {
+                ...state,
+                isLoggedin: false
+            }
+        default: {
+            return {
+                ...state
+            }
+        }
+    }
+}

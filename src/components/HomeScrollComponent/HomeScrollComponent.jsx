0fb38243747afe3f93d1a7c50298e758a23c8c3f
KEEN <kyh0927@live.co.kr>
2019-09-13 23:20:23 +0900

    190913

--- src/components/HomeScrollComponent/HomeScrollComponent.jsx
+++ src/components/HomeScrollComponent/HomeScrollComponent.jsx
@@ -31,25 +31,43 @@ white-space: nowrap;
 `
 
 const LinkItemThumbnail = styled.div `
+    width: 140px;
     border: 1px solid rgba(0,0,0,0.2);
     border-radius: 4px;
-    width: 140px;
+    box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.06);
     font-size: 13px;
     div {
         height: 50px;
+        padding: 10px 0 0 10px;
     }
     img {
         width: 100%;
     }
     b {
         display: block;
+        padding-bottom: 5px;
+    }
+`
+
+const LinkItemThumbnailWrap = styled.div `
+    
+    padding: 0 24px;
+    margin-bottom: 36px;
+    > div {
+        > div {
+            margin: 0 -24px;
+            overflow: hidden;
+        }
     }
 `
 
 const HomeScrollComponent = () => {
     return (
         <>
-            <LinkItemThumbnail>
+            <LinkItemThumbnailWrap>
+            123
+            </LinkItemThumbnailWrap>
+            {/* <LinkItemThumbnail>
                 <picture>
                     <img src="https://a0.muscache.com/4ea/air/v2/pictures/e3b8d12c-07dc-4f3d-9cdc-c79e0a442dd4.jpg?t=r:w375-h250-sfit,e:fjpg-c80" alt="" />
                 </picture>
@@ -58,6 +76,15 @@ const HomeScrollComponent = () => {
                     프랑스
                 </div>
             </LinkItemThumbnail>
+            <LinkItemThumbnail>
+                <picture>
+                    <img src="https://a0.muscache.com/4ea/air/v2/pictures/e3b8d12c-07dc-4f3d-9cdc-c79e0a442dd4.jpg?t=r:w375-h250-sfit,e:fjpg-c80" alt="" />
+                </picture>
+                <div>
+                    <b>파리</b>
+                    프랑스
+                </div>
+            </LinkItemThumbnail>             */}
             {/* <HomeScrollWrap>
                 <HomeScrollXWrap>
                     <HomeScrollX>
